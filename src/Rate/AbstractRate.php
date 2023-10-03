5b9dcb3e9702bf707d68700cce128d5948682951
Ben Griffith <griffith.ben@gmail.com>
2023-10-02 22:22:33 -0500

    AbstractRate refactor on __call method

--- src/Rate/AbstractRate.php
+++ src/Rate/AbstractRate.php
@@ -9,6 +9,9 @@ use Unitz\BaseUnitz;
 
 abstract class AbstractRate
 {
+    private const UNIT_NAME_NUMERATOR = 0;
+    private const UNIT_NAME_DENOMINATOR = 1;
+
     private Inflector $inflector;
     private BaseUnitz $numerator;
     private BaseUnitz $denominator;
@@ -37,19 +40,52 @@ abstract class AbstractRate
     }
 
     /**
-     * @param string $name
+     * @param string $methodName
      * @param array $arguments
      * @return \Unitz\BaseUnitz
+     * @throws \RuntimeException
      */
-    public function __call(string $name, array $arguments): BaseUnitz
+    public function __call(string $methodName, array $arguments): BaseUnitz
+    {
+        $unitNames = $this->getUnitNames($this->checkAndRemoveGetPrefix($methodName));
+
+        $numeratorName = $this->processUnitName($unitNames[self::UNIT_NAME_NUMERATOR]);
+        $denominatorName = $this->processUnitName($unitNames[self::UNIT_NAME_DENOMINATOR]);
+
+        $numerator = $this->getUnitValue($this->numerator, $numeratorName);
+        $denominator = $this->getUnitValue($this->denominator, $denominatorName);
+
+        $numeratorReflection = new \ReflectionClass($this->numerator);
+        $unitsClassName = $numeratorReflection->getName();
+        $unitsClassShortName = $numeratorReflection->getShortName();
+
+        return (new $unitsClassName(preferences: [$unitsClassShortName => $numeratorName]))->{"set$numeratorName"}(
+            $numerator / $denominator
+        );
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     * @throws \RuntimeException
+     */
+    private function checkAndRemoveGetPrefix(string $name): string
     {
         $getPosition = strpos($name, 'get');
         if ($getPosition === false || $getPosition !== 0) {
             throw new RuntimeException('All methods must start with "get"');
         }
 
-        $name = str_replace('get', '', $name);
+        return str_replace('get', '', $name);
+    }
 
+    /**
+     * @param string $name
+     * @return array
+     * @throws \RuntimeException
+     */
+    private function getUnitNames(string $name): array
+    {
         $perPosition = strpos($name, 'Per');
         if ($perPosition === false) {
             throw new RuntimeException('All methods must contain "Per" between the two units');
@@ -64,27 +100,30 @@ abstract class AbstractRate
             );
         }
 
-        $numeratorName = $this->inflector->singularize(ucfirst(strtolower($units[0])));
-        $denominatorName = $this->inflector->singularize(ucfirst(strtolower($units[1])));
+        return $units;
+    }
 
-        if (!method_exists($this->numerator, "get$numeratorName")) {
-            throw new RuntimeException("Method get$numeratorName does not exist on " . get_class($this->numerator));
+    /**
+     * @param string $unitName
+     * @return string
+     */
+    private function processUnitName(string $unitName): string
+    {
+        return $this->inflector->singularize(ucfirst(strtolower($unitName)));
+    }
+
+    /**
+     * @param \Unitz\BaseUnitz $unit
+     * @param string $unitName
+     * @return float
+     * @throws \RuntimeException
+     */
+    private function getUnitValue(BaseUnitz $unit, string $unitName): float
+    {
+        if (!method_exists($unit, "get$unitName")) {
+            throw new RuntimeException("Method get$unitName does not exist on " . get_class($unit));
         }
-        $numerator = $this->numerator->{"get$numeratorName"}();
 
-        if (!method_exists($this->denominator, "get$denominatorName")) {
-            throw new RuntimeException(
-                "Method get$denominatorName does not exist on " . get_class($this->denominator)
-            );
-        }
-        $denominator = $this->denominator->{"get$denominatorName"}();
-
-        $numeratorReflection = new \ReflectionClass($this->numerator);
-        $unitsClassName = $numeratorReflection->getName();
-        $unitsClassShortName = $numeratorReflection->getShortName();
-
-        return (new $unitsClassName(preferences: [$unitsClassShortName => $numeratorName]))->{"set$numeratorName"}(
-            $numerator / $denominator
-        );
+        return $unit->{"get$unitName"}();
     }
 }
